560d33591c63832d80b59ad6710b68149a0d15bc
Kirill <limon17141@gmail.com>
2022-05-28 23:04:38 +0300

    3 practice

--- js/script.js
+++ js/script.js
@@ -18,28 +18,49 @@ const personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');
-    
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('Done!');
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = prompt('На сколько оцените его?', '');
+        
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('Done!');
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
+}
+rememberMyFilms();
+
+function detectPersonalLevel () {
+    if (personalMovieDB.count < 10 ) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Вы классический зритель");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Вы киноман");
     } else {
-        console.log('error');
-        i--;
+        console.log("Прозошла ошибка");
+    }
+}
+detectPersonalLevel();
+
+console.log(personalMovieDB);  
+
+function showMyDB (hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+showMyDB(personalMovieDB.privat);
+
+
+function writeYourGenres () {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номер ${i}`);
     }
 }
 
-if (personalMovieDB.count < 10 ) {
-    console.log("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("Вы классический зритель");
-} else if (personalMovieDB.count >= 30) {
-    console.log("Вы киноман");
-} else {
-    console.log("Прозошла ошибка");
-}
-console.log(personalMovieDB);  
-
-
+writeYourGenres();
